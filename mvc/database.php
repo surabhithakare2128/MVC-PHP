8eee1cc9fa5fde0349a070ba756f9515fd7e3a7a
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-12-13 01:21:59 -0500

    changed database

--- mvc/database.php
+++ mvc/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
-define('CONNECTION', 'sql2.njit.edu');
+define('DATABASE', 'st638');
+define('USERNAME', 'st638');
+define('PASSWORD', 'adhere54');
+define('CONNECTION', 'sql1.njit.edu');
