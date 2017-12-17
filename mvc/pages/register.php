49b92bccdb9afc8dacd5b1cd68d000ea78dd1dc4
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-12-17 18:23:46 -0500

    validating register

--- mvc/pages/register.php
+++ mvc/pages/register.php
@@ -19,13 +19,13 @@
 
 
 <form action="index.php?page=accounts&action=register" method="post">
-    First name: <input type="text" name="fname"><br>
-    Last name: <input type="text" name="lname"><br>
-    Email: <input type="text" name="email"><br>
+    First name: <input type="text" name="fname" required><br>
+    Last name: <input type="text" name="lname" required><br>
+    Email: <input type="email" name="email" required><br>
     Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
+    Birthday: <input type="text" name="birthday" ><br>
     Gender: <input type="text" name="gender"><br>
-    Password: <input type="password" name="password"><br>
+    Password: <input type="password" name="password" required><br>
     <input type="submit" value="REGISTER">
 </form>
 
