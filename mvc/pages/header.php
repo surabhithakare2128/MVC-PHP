a836d8e5809c893218dc2125ff1a5c945085ba94
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-12-17 22:18:40 -0500

    adding css

--- mvc/pages/header.php
+++ mvc/pages/header.php
@@ -2,13 +2,15 @@
 <html lang="en">
 <head>
     <title>WELCOME TO MY WEBSITE</title>
-    <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/background.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 
+<!--<body background="images/background.jpg" height="1000" width="800">
+//<img src="images/background.jpg" class="img-responsive" alt="Image"> -->
+    <br>
     <h1><b><center>WELCOME TO MY WEBSITE!!!</center></b></h1>
 
